756f3480b1b922e9cad198efc0f4cebe0cfb0ad3
digitradiart <digitradiart@gmail.com>
2021-06-20 21:42:24 +0700

    responcive css

--- js/script.js
+++ js/script.js
@@ -56,3 +56,41 @@ window.onload = function () {
   css.innerHTML = '.typewrite > .wrap { border-right: 0.08em solid red}';
   document.body.appendChild(css);
 };
+
+// Adding smooth filling skill capsules behaviour on skills-display visibility
+var skillProgress = document.querySelectorAll('.skill-progress');
+var skillLevel = document.querySelectorAll('.skill-progress>div');
+var skillProgressArr = new Array(skillProgress.length);
+
+for (let i = 0; i < skillProgressArr.length; i++) {
+  skillProgressArr[i] = false;
+}
+// setting initial skill level to 0
+for (let i = 0; i < skillLevel.length; i++) {
+  skillLevel[i].style.width = '0%';
+}
+
+window.addEventListener('scroll', function () {
+  for (let i = 0; i < skillProgress.length; i++) {
+    let skillProgressCoordinates = skillProgress[i].getBoundingClientRect();
+    if (skillProgressCoordinates.y < window.innerHeight && !skillProgressArr[i]) {
+      skillProgressArr[i] = true;
+      let currLevel = 0;
+      let maxLevel = skillLevel[i].getAttribute('data-skill-level');
+      let interval = setInterval(function () {
+        if (currLevel == maxLevel) {
+          clearInterval(interval);
+          return;
+        } else {
+          currLevel++;
+          skillLevel[i].style.width = currLevel + '%';
+        }
+      }, 10);
+    }
+
+    if (skillProgressCoordinates.y >= window.innerHeight) {
+      skillLevel[i].style.width = 0 + '%';
+      skillProgressArr[i] = false;
+    }
+  }
+});
