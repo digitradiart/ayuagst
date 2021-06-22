99986d21e9248bf18fa95a6d5b0aecc6096d547d
digitradiart <digitradiart@gmail.com>
2021-06-23 00:20:50 +0700

    aside

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+window.onscroll = function () { myFunction() };
+
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+
+        function myFunction() {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        }
+
+
 var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -56,41 +70,3 @@ window.onload = function () {
   css.innerHTML = '.typewrite > .wrap { border-right: 0.08em solid red}';
   document.body.appendChild(css);
 };
-
-// Adding smooth filling skill capsules behaviour on skills-display visibility
-var skillProgress = document.querySelectorAll('.skill-progress');
-var skillLevel = document.querySelectorAll('.skill-progress>div');
-var skillProgressArr = new Array(skillProgress.length);
-
-for (let i = 0; i < skillProgressArr.length; i++) {
-  skillProgressArr[i] = false;
-}
-// setting initial skill level to 0
-for (let i = 0; i < skillLevel.length; i++) {
-  skillLevel[i].style.width = '0%';
-}
-
-window.addEventListener('scroll', function () {
-  for (let i = 0; i < skillProgress.length; i++) {
-    let skillProgressCoordinates = skillProgress[i].getBoundingClientRect();
-    if (skillProgressCoordinates.y < window.innerHeight && !skillProgressArr[i]) {
-      skillProgressArr[i] = true;
-      let currLevel = 0;
-      let maxLevel = skillLevel[i].getAttribute('data-skill-level');
-      let interval = setInterval(function () {
-        if (currLevel == maxLevel) {
-          clearInterval(interval);
-          return;
-        } else {
-          currLevel++;
-          skillLevel[i].style.width = currLevel + '%';
-        }
-      }, 10);
-    }
-
-    if (skillProgressCoordinates.y >= window.innerHeight) {
-      skillLevel[i].style.width = 0 + '%';
-      skillProgressArr[i] = false;
-    }
-  }
-});
