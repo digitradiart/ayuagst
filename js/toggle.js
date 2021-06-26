2e334d62a081f18db003fe893acb536e9e25978d
digitradiart <digitradiart@gmail.com>
2021-06-26 20:52:32 +0700

    dark and light theme

--- js/toggle.js
+++ js/toggle.js
@@ -1,4 +1,17 @@
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle('dark-mode');
-}
+// Select the button
+const btn = document.querySelector('.btn-toggle');
+// Select the stylesheet <link>
+const theme = document.querySelector('#theme-link');
+
+// Listen for a click on the button
+btn.addEventListener('click', function () {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute('href') == 'css/light-theme.css') {
+    // ... then switch it to "dark-theme.css"
+    theme.href = 'css/dark-theme.css';
+    // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = 'css/light-theme.css';
+  }
+});
