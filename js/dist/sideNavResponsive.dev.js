e7459a354c85844dd6050cb0463908802526c3e7
digitradiart <digitradiart@gmail.com>
2021-06-27 15:32:26 +0700

    responsive init

---
+++ js/dist/sideNavResponsive.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function openNav() {
+  document.getElementById('contact').style.width = '100%';
+}
+
+function closeNav() {
+  document.getElementById('contact').style.width = '0';
+}
---
+++ js/sideNavResponsive.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById('contact').style.width = '100%';
+}
+
+function closeNav() {
+  document.getElementById('contact').style.width = '0';
+}
