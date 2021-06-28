ada6f096c869cc9170aff97d9ea241096bb716f5
digitradiart <digitradiart@gmail.com>
2021-06-29 00:00:24 +0700

    topbar responsive

--- js/dist/sideNavResponsive.dev.js
+++ js/dist/sideNavResponsive.dev.js
@@ -1,13 +1,20 @@
 "use strict";
 
 function openNav() {
-  // document.getElementById('stickytopbar').style.visibility = 'visible';
+  document.getElementById('stickytopbar').style.visibility = 'visible';
   document.getElementById('stickytopbar').style.background = 'red';
   document.getElementById('stickytopbar').style.width = '100';
 }
 
 function closeNav() {
-  // document.getElementById('stickytopbar').style.visibility = 'hidden';
-  // document.getElementById('stickytopbar').style.display = 'none';
+  document.getElementById('stickytopbar').style.visibility = 'hidden';
+  document.getElementById('stickytopbar').style.display = 'none';
   document.getElementById('stickytopbar').style.width = '0';
-}
+} // function myFunction() {
+//   var x = document.getElementById('stickytopbar');
+//   if (x.className === 'top-navbar') {
+//     x.className += ' responsive';
+//   } else {
+//     x.className = 'top-navbar';
+//   }
+// }
--- js/sideNavResponsive copy.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-  document.getElementById('contact').style.width = '60%';
-}
-
-function closeNav() {
-  document.getElementById('contact').style.width = '0';
-}
