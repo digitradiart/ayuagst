71231208bd1c7bf98ad5d04ee9b7f0ed4571a2a8
digitradiart <digitradiart@gmail.com>
2021-07-13 04:24:04 +0700

    fix

--- js/dist/responsiveNavbar.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// INTERACTION ON HAMBURGER MENU
-var menuToggle = document.querySelector('.menu-toggle input');
-var nav = document.querySelector('#stickytopbar ul');
-menuToggle.addEventListener('click', function () {
-  nav.classList.toggle('slide');
-});
